217d747c2834af2ad681a751065d5b1243f72d66
Carlos Castillo <cmaxhm@yahoo.com>
2026-02-22 15:27:21 -0500

    Add configuration server

---
+++ configuration-server/src/test/java/com/cmaxhm/configurationserver/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmaxhm.configurationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
