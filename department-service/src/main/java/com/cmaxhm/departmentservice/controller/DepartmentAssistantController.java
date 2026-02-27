ba619985e3a9d335604069beb81d47f2232efd6b
Carlos Castillo <cmaxhm@yahoo.com>
2026-02-27 15:00:26 -0500

    Add Actuator refresh functionality for department service to support dynamic configuration updates

---
+++ department-service/src/main/java/com/cmaxhm/departmentservice/controller/DepartmentAssistantController.java
@@ -0,0 +1,21 @@
+package com.cmaxhm.departmentservice.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/departments/assistant")
+@RefreshScope
+public class DepartmentAssistantController {
+  @Value("${default.message}")
+  private String defaultMessage;
+
+  @GetMapping("/hello")
+  public ResponseEntity<String> hello() {
+    return ResponseEntity.ok(this.defaultMessage);
+  }
+}
